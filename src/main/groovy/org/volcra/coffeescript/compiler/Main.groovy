1318face59e8646b321319a547b248d91ffe8017
Emanuelle Garduno <emanuelle.garduno@gmail.com>
2013-04-26 09:22:19 -0400

    Copyright and License

--- src/main/groovy/org/volcra/coffeescript/compiler/Main.groovy
+++ src/main/groovy/org/volcra/coffeescript/compiler/Main.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013 Volcra
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.volcra.coffeescript.compiler
 
 /**
@@ -20,7 +35,7 @@ class Main {
      * Parsers the command line arguments and runs the CoffeeScript compiler.
      */
     static void main(String... args) {
-        def options = cli.parse(args);
+        def options = cli.parse args
 
         if (options.arguments().isEmpty() || options.h) {
             cli.usage()
